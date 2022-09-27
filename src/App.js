24cd838db01bb4f7561223b8159f7d0857e73c4c
dhanashree-wagh <84534754+dhanashree-wagh@users.noreply.github.com>
2022-09-27 21:35:44 +0530

    final
    s

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from "react";
-// import { BrowserRouter, Route, Navigate , Routes } from "react-router-dom";
+import { BrowserRouter, Route, Navigate , Routes } from "react-router-dom";
 import UserPage from "./containers/UserPage";
 
 import "./App.css";
 
 function App() {
-  // const username = prompt("Enter Github Username");
   
-
   return (
-    <UserPage/>
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/:userName" component={UserPage} />
-    //     {/* <Redirect to={`/${username}`} /> */}
-    //     <Route path='/' element={<Navigate replace to='/${username}' />} />
-    //   </Routes>
-    // </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:userName" component={UserPage} />
+        {/* <Redirect to={`/${username}`} /> */}
+        <Route path='/' element={<Navigate replace to='/${username}' />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
